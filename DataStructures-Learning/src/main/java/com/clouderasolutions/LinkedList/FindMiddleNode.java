ec36bbdb3c6323d3f6be92477629ffc442201f58
Anuj Walia <waliaanuj1990@gmail.com>
2017-11-17 14:56:52 +0530

    added code for FindMiddleNode

--- DataStructures-Learning/src/main/java/com/clouderasolutions/LinkedList/FindMiddleNode.java
+++ DataStructures-Learning/src/main/java/com/clouderasolutions/LinkedList/FindMiddleNode.java
@@ -1,10 +1,51 @@
 package com.clouderasolutions.LinkedList;
 
+import org.apache.log4j.Logger;
+
 public class FindMiddleNode {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static final Logger LOG = Logger.getLogger(FindMiddleNode.class);
 
+	static class Node {
+		int data;
+		Node next;
+
+		Node(int data) {
+			this.data = data;
+		}
+
+		@Override
+		public String toString() {
+
+			return Integer.toString(data);
+		}
+	}
+
+	public Node findMiddleNode(Node head) {
+		Node slowPointer = head;
+		Node fastPointer = head;
+
+		while (fastPointer.next != null) {
+			LOG.debug("SlowPointer Value : " + slowPointer);
+			LOG.debug("fastPointer Value : " + fastPointer);
+			slowPointer = slowPointer.next;
+			fastPointer = fastPointer.next.next;
+		}
+
+		return slowPointer;
+	}
+
+	public static void main(String[] args) {
+		LOG.info("Adding nodes to list");
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next = new Node(4);
+
+		LOG.info("Calling findMiddleNode method");
+		FindMiddleNode findMiddleNode = new FindMiddleNode();
+
+		LOG.info(findMiddleNode.findMiddleNode(head));
 	}
 
 }
